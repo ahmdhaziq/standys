2b6ca7869de9e31cf7fc304a9777ea873f0fa0f9
Ahmad Haziq <ahmadhaziq2600@gmail.com>
2026-08-24 13:30:16 +0800

    fix: fix module tasks

--- backend/core/src/modules/tasks/tasks.module.ts
+++ backend/core/src/modules/tasks/tasks.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { TasksController } from './tasks.controller';
 import { PrismaModule } from 'src/infrastructure/prisma/prisma.module';
+import { TasksRepository } from './tasks.repository';
 
 @Module({
-  providers: [TasksService],
+  providers: [TasksService, TasksRepository],
   controllers: [TasksController],
   imports: [PrismaModule],
 })
